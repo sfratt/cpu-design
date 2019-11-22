321e4097444c1a63c4427747cccdc979a29cf7a9
agrek <agrek@users.noreply.github.com>
2019-11-22 18:20:10 -0500

    renamed var names in data_cache to conform to naming conventions from my datapath

--- vhdl/data_cache.vhd
+++ vhdl/data_cache.vhd
@@ -3,12 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity data_cache is
     port(
-        d_in : in std_logic_vector(31 downto 0);
+        data_in : in std_logic_vector(31 downto 0);
         address : in std_logic_vector(4 downto 0);
         clk : in std_logic;
-        reset : in std_logic;
+        rst : in std_logic;
         data_write : in std_logic;
-        d_out : out std_logic_vector(31 downto 0)
+        data_out : out std_logic_vector(31 downto 0)
     );
 end data_cache;
 
