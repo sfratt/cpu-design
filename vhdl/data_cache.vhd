4792491db4582e8bdab529a1123c85ad9a945ecb
sfratt <sfratt@users.noreply.github.com>
2019-11-26 22:20:21 -0500

    Add ports to process sensitivity list

--- vhdl/data_cache.vhd
+++ vhdl/data_cache.vhd
@@ -4,12 +4,12 @@ use ieee.std_logic_unsigned.all;
 
 entity data_cache is
     port(
-        d_in        : in std_logic_vector(31 downto 0);
-        address     : in std_logic_vector(4 downto 0);
-        clk         : in std_logic;
-        reset         : in std_logic;
-        data_write  : in std_logic;
-        d_out       : out std_logic_vector(31 downto 0)
+        d_in       : in std_logic_vector(31 downto 0);
+        address    : in std_logic_vector(4 downto 0);
+        clk        : in std_logic;
+        reset      : in std_logic;
+        data_write : in std_logic;
+        d_out      : out std_logic_vector(31 downto 0)
     );
 end data_cache;
 
@@ -18,7 +18,7 @@ architecture d_cache_arch of data_cache is
     signal loc : locations;
 begin
 
-    data_cache_unit : process(clk, reset, loc)
+    data_cache_unit : process(reset, clk, loc, d_in, address)
     begin
         if reset = '1' then
             for i in 0 to 31 loop
